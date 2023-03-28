d9beb1b6e5b4b3af8f4822f9ff568d96f526f59b
Nfassory Diaby <fassory.diaby@gmail.com>
2023-03-28 18:17:22 +0200

    OffersFixtures

---
+++ src/DataFixtures/OffersFixtures.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Offers;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class OffersFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+        $offers = $manager->getRepository(Offers::class)->findAll();
+
+        $object = (new Offers())
+            ->setTitle('dentaire')
+            ->setLink($faker->words($faker->numberBetween(1, 5), true))
+        ;
+
+        $manager->persist($object);
+
+        $object = (new Offers())
+            ->setTitle('optique')
+            ->setLink($faker->words($faker->numberBetween(1, 5), true))
+        ;
+
+        $manager->persist($object);
+
+        $object = (new Offers())
+            ->setTitle('général')
+            ->setLink($faker->words($faker->numberBetween(1, 5), true))
+        ;
+
+        $manager->persist($object);
+
+
+        $manager->flush();
+    }
+}

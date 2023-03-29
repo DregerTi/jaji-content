87e71011c398af075247de0360b86a85b6b3167e
LouisAntoine <louisantoine.pro@gmail.com>
2023-03-29 15:00:03 +0200

    Front improvement + header finished

--- migrations/Version20230328171435.php
+++ migrations/Version20230328171435.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230329102009 extends AbstractMigration
+final class Version20230328171435 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,13 +20,15 @@ final class Version20230329102009 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE contents ALTER title TYPE VARCHAR(80)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_3AF34668EA750E8 ON categories (label)');
+        $this->addSql('ALTER TABLE contents ALTER prewiew_img SET NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE contents ALTER title TYPE VARCHAR(255)');
+        $this->addSql('DROP INDEX UNIQ_3AF34668EA750E8');
+        $this->addSql('ALTER TABLE contents ALTER prewiew_img DROP NOT NULL');
     }
 }

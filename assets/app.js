ebe4c9163efe4a44e461d6a38471c0cc79c69e09
DregerTi <72619834+DregerTi@users.noreply.github.com>
2023-03-29 17:04:07 +0200

    infinite scroll

--- assets/app.js
+++ assets/app.js
@@ -1,29 +1,10 @@
 import './styles/app.scss';
 
+// import if you use the NPM package (not needed if you use CDN)
 import InfiniteAjaxScroll from '@webcreate/infinite-ajax-scroll';
 
-let ias = new InfiniteAjaxScroll('#containerFeed', {
+let ias = new InfiniteAjaxScroll('.containerFeed', {
     item: '.content-preview',
-    next: '.pager__next',
-    pagination: '.pager',
-
-    // false so we can bind manually after all images are loaded
-    // -- if we didn't wait for images to load, the content would be
-    // too short and IAS would try to load the next page right away
-    bind: false
-});
-
-// manually hide the pagination because IAS isn't binded yet
-document.querySelector('.pager').style.display = 'none';
-
-
-ias.on('append', (event) => {
-    let appendFn = event.appendFn;
-
-    event.appendFn = (items, parent, last) => {
-        return new Promise((resolve) => {
-            appendFn(items, parent, last);
-
-        });
-    };
+    next: '.next',
+    pagination: '.pager'
 });

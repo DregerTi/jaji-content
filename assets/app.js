41e5c8232ec9a5da9db93fa13213258da5ed5ddc
DregerTi <72619834+DregerTi@users.noreply.github.com>
2023-03-30 18:14:01 +0200

    css fixes

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
 import './styles/app.scss';
-
+import './js/notyf.js';
 // import if you use the NPM package (not needed if you use CDN)
 import InfiniteAjaxScroll from '@webcreate/infinite-ajax-scroll';
 
